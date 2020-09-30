c4ec2486258baa79dbbfe5216b634753606e4dcd
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-09-30 22:28:54 +0300

    build.gradle.kts prettied and formatted.

--- generic/build.gradle.kts
+++ generic/build.gradle.kts
@@ -3,15 +3,18 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.4.10"
 }
+
 group = "com.planningwisely"
 version = "1.0.0-SNAPSHOT.1"
 
 repositories {
     mavenCentral()
 }
+
 dependencies {
     testImplementation(kotlin("test-junit5"))
 }
+
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
 }
