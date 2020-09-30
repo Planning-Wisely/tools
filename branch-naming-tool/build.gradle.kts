a683bcb7863a046e628c1eb2d24f8413be77a97f
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-09-30 22:26:42 +0300

    Changed project structure. Closed #1.

---
+++ branch-naming-tool/build.gradle.kts
@@ -0,0 +1,22 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.10"
+    application
+}
+group = "com.planningwisely"
+version = "1.0.0-SNAPSHOT.1"
+
+repositories {
+    mavenCentral()
+}
+dependencies {
+    implementation(project(":generic"))
+    testImplementation(kotlin("test-junit5"))
+}
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
+application {
+    mainClassName = "MainKt"
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,7 @@
 
 rootProject.name = "tools"
 
+
+include(":branch-naming-tool")
+include(":generic")
+
