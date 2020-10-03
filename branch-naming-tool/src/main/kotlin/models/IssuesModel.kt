fd223e94d2021b4a1f3dd6fb3761e311e99c6356
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-10-04 01:07:18 +0300

    IssuesModel.kt removed.

--- branch-naming-tool/src/main/kotlin/models/IssuesModel.kt
+++
@@ -1,9 +0,0 @@
-package models
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-class IssuesModel(val entry: List<IssuesModelItem> = listOf()) {
-    @Serializable
-    data class IssuesModelItem(val number: Int = 0, val title: String = "", val url: String = "")
-}
